d654d6ebc91f188152090ab831790b14f940d68c
EricJoyBoy <ericonelopes@gmail.com>
2021-09-09 00:02:43 +0200

    vamo ver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CurriculumComponent } from './components/template/curriculum/curriculum.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { ProgettiComponent } from './components/template/progetti/progetti.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:HeaderComponent
+  },
+  {
+    path:"curriculum",
+    component:CurriculumComponent
+  },
+  {
+    path:"progetti",
+    component:ProgettiComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FooterComponent } from './components/template/footer/footer.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { CurriculumComponent } from './components/template/curriculum/curriculum.component';
+import { ProgettiComponent } from './components/template/progetti/progetti.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   HeaderComponent,
+    FooterComponent,
+    CurriculumComponent,
+    ProgettiComponent,
+    
   ],
   imports: [
     BrowserModule,
